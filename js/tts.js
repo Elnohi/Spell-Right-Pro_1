5b0530b71cef6cab58f634746559fdb45a2a0b1b
Elnohi <samielnohi@yahoo.com>
2025-07-15 16:08:53 -0400

    Update tts.js

--- js/tts.js
+++ js/tts.js
@@ -1,1 +1,6 @@
-
+function speakWord() {
+  const utterance = new SpeechSynthesisUtterance(currentWord);
+  const lang = document.getElementById("accent-select")?.value || "en-US";
+  utterance.lang = lang;
+  speechSynthesis.speak(utterance);
+}
