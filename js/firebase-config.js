13bace57b8547994631af1d95b2df6ed242cc0cf
Elnohi <samielnohi@yahoo.com>
2025-07-16 01:04:13 -0400

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,11 +1,14 @@
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCZ-rAPnRgVjSRFOFvbiQlowE6A3RVvwWo",
+  authDomain: "spellrightpro-firebase.firebaseapp.com",
+  projectId: "spellrightpro-firebase",
+  storageBucket: "spellrightpro-firebase.appspot.com",
+  messagingSenderId: "798456641137",
+  appId: "1:798456641137:web:5c6d79db5bf49d04928dd0",
+  measurementId: "G-H09MF13297"
 };
+
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
