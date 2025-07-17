db846a8a133d14e9ae3b0a8755d68e091fa3da75
Elnohi <samielnohi@yahoo.com>
2025-07-17 19:09:47 -0400

    Delete js/tts.js

--- js/tts.js
+++
@@ -1,6 +0,0 @@
-function speakWord() {
-  const utterance = new SpeechSynthesisUtterance(currentWord);
-  const lang = document.getElementById("accent-select")?.value || "en-US";
-  utterance.lang = lang;
-  speechSynthesis.speak(utterance);
-}
