733f5e6c74b4c6d364363e8bb16ba8d8aaa3e524
Elnohi <samielnohi@yahoo.com>
2025-07-19 09:50:41 -0400

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,21 @@
+// config.js - Shared configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyCZ-rAPnRgVjSRFOFvbiQlowE6A3RVvwWo",
+  authDomain: "spellrightpro-firebase.firebaseapp.com",
+  projectId: "spellrightpro-firebase",
+  storageBucket: "spellrightpro-firebase.appspot.com",
+  messagingSenderId: "798456641137",
+  appId: "1:798456641137:web:5c6d79db5bf49d04928dd0",
+  measurementId: "G-H09MF13297"
+};
+
+export const defaultWords = {
+  OET: [
+    "Scalp", "Eczema", "Osteoarthritis", "Atrial fibrillation", "Fatigue",
+    "Constipation", "Bruising", "Lying down", "Autism", "Insomnia"
+  ],
+  SPELLING_BEE: [
+    "articulate", "pharaoh", "onomatopoeia", "surveillance",
+    "metamorphosis", "onomastics", "entrepreneur", "mnemonic"
+  ]
+};
