c8c8f824fd55d44fecdd1539351631dfb0f3e34e
Elnohi <samielnohi@yahoo.com>
2025-07-22 16:43:24 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,33 +1,9 @@
-// Shared configuration constants
-export const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyCZ-rAPnRgVjSRFOFvbiQlowE6A3RVvwWo",
   authDomain: "spellrightpro-firebase.firebaseapp.com",
   projectId: "spellrightpro-firebase",
-  storageBucket: "spellrightpro-firebase.appspot.com",
+  storageBucket: "spellrightpro-firebase.firebasestorage.app",
   messagingSenderId: "798456641137",
   appId: "1:798456641137:web:5c6d79db5bf49d04928dd0",
   measurementId: "G-H09MF13297"
 };
-
-export const examTypes = {
-  OET: 'oet',
-  SPELLING_BEE: 'spellingbee',
-  CUSTOM: 'custom'
-};
-
-export const defaultWords = {
-  [examTypes.OET]: [
-    "Scalp", "Eczema", "Osteoarthritis", "Atrial fibrillation", "Fatigue",
-    "Constipation", "Bruising", "Lying down", "Autism", "Insomnia"
-  ],
-  [examTypes.SPELLING_BEE]: [
-    "articulate", "pharaoh", "onomatopoeia", "surveillance",
-    "metamorphosis", "onomastics", "entrepreneur", "mnemonic"
-  ]
-};
-
-export const accentOptions = [
-  { value: 'en-US', label: 'American English' },
-  { value: 'en-GB', label: 'British English' },
-  { value: 'en-AU', label: 'Australian English' }
-];
