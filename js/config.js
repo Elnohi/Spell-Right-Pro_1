7feeff1ac6bd916fcbd453a219a22d2d5a7bd8ce
Elnohi <samielnohi@yahoo.com>
2025-07-23 12:30:39 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
-window.firebaseConfig = {
+// js/config.js
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
   apiKey: "AIzaSyCZ-rAPnRgVjSRFOFvbiQlowE6A3RVvwWo",
   authDomain: "spellrightpro-firebase.firebaseapp.com",
   projectId: "spellrightpro-firebase",
@@ -7,3 +9,6 @@ window.firebaseConfig = {
   appId: "1:798456641137:web:5c6d79db5bf49d04928dd0",
   measurementId: "G-H09MF13297"
 };
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
