c16191656e81756389cf7554c076edc251a2fe27
Elnohi <samielnohi@yahoo.com>
2025-07-27 00:48:25 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,14 +1,18 @@
-// js/config.js
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCZ-rAPnRgVjSRFOFvbiQlowE6A3RVvwWo",
   authDomain: "spellrightpro-firebase.firebaseapp.com",
   projectId: "spellrightpro-firebase",
-  storageBucket: "spellrightpro-firebase.firebasestorage.app",
+  storageBucket: "spellrightpro-firebase.appspot.com",
   messagingSenderId: "798456641137",
   appId: "1:798456641137:web:5c6d79db5bf49d04928dd0",
-  measurementId: "G-H09MF13297"
+  measurementId: "G-H09MF13297" // This enables Analytics automatically
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-const db = firebase.firestore();
+
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = firebase.analytics(); // Get analytics instance
+
+// Enable debug mode during development
+if (window.location.hostname === "localhost") {
+  analytics.setAnalyticsCollectionEnabled(true);
+}
