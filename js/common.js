ac0a9817360ae6bd4d73fba73c978e34edf590b8
Elnohi <samielnohi@yahoo.com>
2025-08-10 01:07:25 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -1,17 +1,6 @@
 // common.js - Shared Core v2.4 (Full Feature Set)
 
 // ======================
-// Firebase Services
-// ======================
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
-};
 
 let firebaseApp;
 let auth;
