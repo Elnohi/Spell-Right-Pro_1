dda5881bc02d2d46c59742385d082d172c47d0b4
Elnohi <samielnohi@yahoo.com>
2025-08-27 02:16:56 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -104,18 +104,18 @@
       if (!footer) return;
 
       var map = {
-        'terms':        root + '/terms.html',
-        'privacy':      root + '/privacy.html',
-        'refund-policy':root + '/refund-policy.html',
-        'contact':      root + '/contact.html'
+        'terms':         root + '/terms.html',
+        'privacy':       root + '/privacy.html',
+        'refund-policy': root + '/refund-policy.html',
+        'contact':       root + '/contact.html'
       };
 
       footer.querySelectorAll('a[href]').forEach(function(a){
         var href = a.getAttribute('href') || '';
-        if (/^\/?terms(\.html)?$/i.test(href))            a.href = map['terms'];
-        else if (/^\/?privacy(\.html)?$/i.test(href))     a.href = map['privacy'];
+        if (/^\/?terms(\.html)?$/i.test(href))             a.href = map['terms'];
+        else if (/^\/?privacy(\.html)?$/i.test(href))      a.href = map['privacy'];
         else if (/^\/?refund-?policy(\.html)?$/i.test(href)) a.href = map['refund-policy'];
-        else if (/^\/?contact(\.html)?$/i.test(href))     a.href = map['contact'];
+        else if (/^\/?contact(\.html)?$/i.test(href))      a.href = map['contact'];
       });
     }
     if (document.readyState === 'loading') {
@@ -125,3 +125,4 @@
     }
   })();
 
+})();
